ba9bcc3ff4689e3b2fe4362e685198691ebae2ff
Estuardomomo <estuardocorreos@gmail.com>
2018-04-11 15:45:27 -0600

    Parse design
    empty parse class was designed

---
+++ VMTranslator/src/Parser.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User_Len
+ */
+public class Parser {
+    public Parser(String address){
+    
+    }
+    //returns the type of the current vm command
+    private String commandType(String command){
+    return "";
+    }
+    //Returns the first argument of the current command
+    private String arg1(){
+    return "";
+    }
+    //Returns the second argument of the current command
+    private int arg2(){
+    return 0;
+    }
+    //
+}
