e519422f3c3f0fda3966094091be7094d5eea578
Estuardomomo <estuardocorreos@gmail.com>
2018-04-11 16:07:25 -0600

    CodeWriter design
    Empty code writer class was designed

--- VMTranslator/src/code_writer.java
+++ VMTranslator/src/code_writer.java
@@ -9,5 +9,23 @@
  * @author User_Len
  */
 public class code_writer {
+    public code_writer(){
+    }
+    //informs the code writer that the translation of a new VM file has started
+    public void setFileName(String fileName){
     
+    }
+    //writes the assembly code that is the translation of the given arithmetic command
+    public void writeArithmetic(String command){
+    
+    }
+    //writes the assembly code that is the translation of the given command
+    public void writePushPop(String command, String segment, int index){
+    
+    }
+    //closes the output file
+    public void close()
+    {
+        
+    }
 }
